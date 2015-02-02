929de9cd262c9ed89fc0529866385e7fcecc2d0e
Carlos Guerra <carlos@veinteligente.org>
2015-02-02 17:45:36 -0430

    iniciaalizacion con problemas de user corregido y se agrega movistar a wvdial.conf

--- inicializacion.sh
+++ inicializacion.sh
@@ -1,9 +1,10 @@
 sudo apt-get remove wolfram-engine scratch sonic-pi
 sudo apt-get update
 sudo apt-upgrade
-sudo apt-get install python-pip python-setuptools python-dnspython python-argparse python-m2crypto ppp usb-modeswitch wvdial
-pip install requests
-pip install centinel-dev==0.1.5.1
+sudo apt-get install python-pip python-setuptools python-dnspython python-argparse python-m2crypto ppp usb-modeswitch wvdial procmail
+sudo pip install requests
+sudo pip install centinel-dev==0.1.5.1
 centinel-dev --sync
 centinel-dev
 cp otros/config.ini ~/.centinel/config.ini
+
