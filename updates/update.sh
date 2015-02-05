6d7aac5e820fa32b5e001f0e21a42d36a3d37d2a
mfelaco <mfelaco26@gmail.com>
2015-02-05 13:31:55 -0430

    MF: Agregado cron update. Cambiadas rutas a absolutas.

--- updates/update.sh
+++ updates/update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 id_update_server=$(curl -L -s http://creceiver.guerracarlos.com/updates/id_update.txt)
 echo "Id en server: " $id_update_server
-id_update_aguila=$(cat ~/archivos_locales/id_update)
+id_update_aguila=$(cat /home/pi/archivos_locales/id_update)
 echo "Id en aguila: " $id_update_aguila
-lockfile=~/vesinfiltro.lock
+lockfile=/home/pi/vesinfiltro.lock
 if [ "$id_update_server" == "$id_update_aguila" ]
 then
     echo "No hay updates pendientes"
@@ -17,15 +17,15 @@ else
         script_pendiente=http://creceiver.guerracarlos.com/updates/"$c".sh
         echo "$script_pendiente"
         #armar el archivo de scripts correspondiente
-        wget "$script_pendiente" -P ~/archivos_locales/updates/
-        sh ~/archivos_locales/updates/"$c".sh
-        echo "$c" > ~/archivos_locales/id_update
+        wget "$script_pendiente" -P /home/pi/archivos_locales/updates/
+        sh /home/pi/archivos_locales/updates/"$c".sh
+        echo "$c" > /home/pi/archivos_locales/id_update
     done
     #lockfile
-    rm -f ~/vesinfiltro.lock
+    rm -f /home/pi/vesinfiltro.lock
     trap - INT TERM EXIT
     #cambiar id en aguila
-    #~/archivos_locales/id_update << $id_update_server
+    #/home/pi/archivos_locales/id_update << $id_update_server
     echo "actualizado"
-    cat ~/archivos_locales/id_update
+    cat /home/pi/archivos_locales/id_update
 fi
